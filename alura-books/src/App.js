2843e034f530d931fd8bbc2d5f56e20ee30b76f2
luckmigz <lucas.matosnegri@hotmail.com>
2023-08-29 17:04:59 -0300

    continuar aulas

---
+++ alura-books/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import logo from './imagens/logo.svg';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        div className = "logo" >
+        <
+        img src = { logo }
+        alt = 'logo' / >
+        <
+        p > < strong > Alura < /strong> Books </p >
+        <
+        /div> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
