b276b0ef4d8df63191ce917b1a88818a738eca4c
luckmigz <lucas.matosnegri@hotmail.com>
2023-09-11 14:29:16 -0300

    Concertado
    :wq

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -3,20 +3,6 @@ import Pesquisa from './componentes/Pesquisa'
 import UltimosLancamentos from './componentes/UltimosLancamentos'
 import styled from 'styled-components'
 
-const express = require('express')
-
-const app = express()
-
-const port = 8000
-
-app.get('/', (req, res) => {
-  res.send('Olá mundo!')
-})
-
-app.listen(port, () => {
-  console.log(`Escutando a porta ${port}`)
-})
-
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
