10b9ed4335a070a6483c43a7994303300445558b
luckmigz <lucas.matosnegri@hotmail.com>
2023-09-25 17:01:50 -0300

    Aula 02 concluida

--- alura-books-server/app.js
+++ alura-books-server/app.js
@@ -1,13 +1,16 @@
 //Import do express 
 const express = require("express")
-
 //Import do livros.js
 const rotaLivro = require("./rotas/livros.js")
+// Import do CORS - Cross-origin Resource Sharing
+const cors = require("cors")
 
 // Criando uma aplicação express
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({origin: "*"}))
 
 app.use('/livros', rotaLivro)
 
